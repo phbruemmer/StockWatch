22c935c94985f776822f1fd905601d106b3f1e23
phbruemmer <phbruemmer@gmail.com>
2024-10-19 23:58:13 +0200

    - Added functioning input fields -> Proper login logic will be implemented in the next few days...

--- login/views.py
+++ login/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def index(requests):
+    if requests.method == 'POST':
+        email = requests.POST.get('email')
+        password = requests.POST.get('password')
+        print(email)
+        print(password)
     return render(requests, 'login_index.html')
