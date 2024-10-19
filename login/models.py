e36234c808a8cc2c2c9403e00e734d8b977e1457
phbruemmer <phbruemmer@gmail.com>
2024-10-20 01:24:06 +0200

    - Added Database model for User instance

--- login/models.py
+++ login/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=40)
+    password = models.CharField(max_length=42)
--- login/views.py
+++ login/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+import hashlib
+from login.models import *
 
 
 def index(requests):
     if requests.method == 'POST':
         email = requests.POST.get('email')
         password = requests.POST.get('password')
+        password = hashlib.sha256(password.encode()).hexdigest()
         print(email)
         print(password)
+        print(User.objects.all())
+
     return render(requests, 'login_index.html')
